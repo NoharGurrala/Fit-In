95da32fcfb3e11023a6224b9670688ab11fef001
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-05-13 00:51:51 -0700

    axios

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    BASE_URL: "http://localhost:3001"
+    baseURL: "http://localhost:3001"
 });
 
 axiosInstance.defaults.headers.common["Authorization"] = localStorage.getItem('token');
